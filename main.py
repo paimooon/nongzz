e88194f32d595679822f94eb7f66fa27a8cb59a9
paimooon <81897465+paimooon@users.noreply.github.com>
2023-02-26 08:53:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,23 @@ from proto import QueryCurrRegionHttpRsp_pb2
 if __name__ == '__main__':
 
     parseList = [
+        "00/24230448",
         "00/25539185",
         "01/26692920",
-        "09/22299426"
+        "02/27251172",
+        "03/25181351",
+        "04/25776943",
+        "05/20618174",
+        "06/25555476",
+        "07/30460104",
+        "08/32244380",
+        "09/22299426",
+        "10/23331191",
+        "11/21030516",
+        "12/32056053",
+        "13/34382464",
+        "14/27270675",
+        "15/21419401"
     ]
 
     output = base64.b64decode(enc.decrypt(requests.get(os.environ.get('URL')).text))
@@ -21,3 +35,12 @@ if __name__ == '__main__':
         os.makedirs("./blk/", exist_ok=True)
         with open("./blk/" + i.split("/")[1] + ".blk", "wb") as file:
             file.write(requests.get(url).content)
+    
+    url = f"{curr.region_info.resource_url}/output_{curr.region_info.res_version_config.version}_{curr.region_info.res_version_config.version_suffix}/client/StandaloneWindows64/AssetBundles/blocks/31049740.blk"
+    
+    print(url)  
+
+    os.makedirs("./blk/", exist_ok=True)
+    with open("./blk/" + i.split("/")[1] + ".blk", "wb") as file:
+        file.write(requests.get(url).content)
+    
